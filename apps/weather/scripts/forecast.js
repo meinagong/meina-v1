f42a3d5c1846c9ea1458edca9c2997644baa2456
meinagong <meinagong@gmail.com>
2021-06-02 13:17:37 +0800

    edited http to https

--- apps/weather/scripts/forecast.js
+++ apps/weather/scripts/forecast.js
@@ -3,7 +3,7 @@ const key = '4GRF8nFtyPRUkVXzh4ZSI41Ax6Lv6pwI';
 // get weather information
 const getWeather = async (id) => {
   
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
@@ -16,7 +16,7 @@ const getWeather = async (id) => {
 // get city information
 const getCity = async (city) => {
 
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
