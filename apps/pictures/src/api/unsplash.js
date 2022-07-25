db1cf571bdd79c871812f11c95357e7402c80033
meinagong <meinagong@gmail.com>
2022-07-26 00:08:11 +0800

    solve buil fail

--- apps/pictures/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID IcTWI1iyyjEG6IH-4xkM_en5AGWHhYQmv-ZLkPMvYpw'
-  }
-});
